7a96917e962f7960a3097f27821a4cc18ef3d132
taminif <taminif@gmail.com>
2026-02-28 18:26:59 +0900

    first commit

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'poker-memo-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
+  );
+});
